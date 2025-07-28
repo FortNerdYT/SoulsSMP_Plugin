b04921400a48d3c78a9daeb3bbc4a6e41cd84d85
FortNerdYT <elhodovan1@gmail.com>
2025-07-28 16:25:42 -0400

    update

--- src/main/java/com/yourname/soulplugin/enums/SoulRarity.java
+++ src/main/java/com/yourname/soulplugin/enums/SoulRarity.java
@@ -3,10 +3,10 @@ package com.yourname.soulplugin.enums;
 import org.bukkit.ChatColor;
 
 public enum SoulRarity {
-    COMMON(ChatColor.WHITE, 20.0),
-    RARE(ChatColor.BLUE, 35.0),
+    COMMON(ChatColor.WHITE, 40.0),
+    RARE(ChatColor.BLUE, 30.0),
     MYTHIC(ChatColor.DARK_PURPLE, 20.0),
-    GOD_TIER(ChatColor.GOLD, 5.0),
+    GOD_TIER(ChatColor.GOLD, 10.0),
     EVENT(ChatColor.LIGHT_PURPLE, 0.0); // Event souls don't drop naturally
     
     private final ChatColor color;
